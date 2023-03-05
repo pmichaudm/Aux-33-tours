e1488479b26b75778804a550eb7412308a265b98
pmichaudm <philippe.mm@hotmail.com>
2023-03-05 00:36:43 -0500

    Added a scheduler to update the records every day

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 import os
+import threading
 import nextcord
 from nextcord import Interaction
 from nextcord.ext import commands
+import schedule
+import time
+from scripts.write_new_arrivals import WriteNewVinyl
+from scripts.write_new_arrivals_used import WriteUsedVinyl
+from scripts.Timer import Timer
 
 TOKEN = 'token.txt'
 command_prefix = '/'
@@ -13,9 +19,33 @@ def token():
     with open(TOKEN, "r") as f:
         return f.read()
 
+
 def admin():
     with open("admin.txt", "r") as f:
         return f.read()
+
+
+def scheduled_update():
+    print("Update scheduler started on thread: " + threading.current_thread().name)
+    timer = Timer()
+
+    def writeNewRecords():
+        print("Writing new records")
+        timer.start()
+        WriteNewVinyl()
+        print(f"Finished writing new records in {Timer.stop:0.4f} seconds.")
+
+    def writeUsedRecords():
+        print("Writing new records")
+        timer.start()
+        WriteUsedVinyl()
+        print(f"Finished writing used records in {Timer.stop:0.4f} seconds.")
+
+    schedule.every().day.at("18:30").do(writeNewRecords)
+    schedule.every().day.at("18:50").do(writeUsedRecords)
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
 
 
 @client.event
@@ -57,6 +87,14 @@ async def reload(interaction: Interaction, extension):
             await interaction.send(f"Error reloading {extension} cog: {e}")
 
 
+@client.command()
+async def ping(interaction: Interaction):
+    for guild in client.guilds:  # guild stands for server
+        for channel in guild.channels:
+            if isinstance(channel, nextcord.TextChannel):  # Check if channel is a text channel
+                await channel.send("Hi")
+
+
 initial_extensions = []
 for fn in os.listdir("cogs"):
     if fn.endswith(".py"):
@@ -66,6 +104,5 @@ if __name__ == "__main__":
     for extension in initial_extensions:
         client.load_extension(extension)
         print(f"Loaded {extension} cog!")
+    threading.Thread(target=scheduled_update).start()
     client.run(token())
-
-
